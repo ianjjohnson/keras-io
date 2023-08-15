ianjjohnson/keras-io

commit 88f51034b4e9f473e5d45247fc06fdc95108f7ec

Updated use of embed_dim and ff_dim arguments to match expected sshape implications within the transformer block. (#669)
Co-authored-by: Luke Walters <luke.walters@luminouscomputing.com>

examples/nlp/text_classification_with_switch_transformer.py
@@ -92,9 +92,9 @@ This is used as the Mixture of Experts in the Switch Transformer.
 """
 
 
-def create_feedforward_network(ff_dim, name=None):
+def create_feedforward_network(ff_dim, embed_dim, name=None):
     return keras.Sequential(
-        [layers.Dense(ff_dim, activation="relu"), layers.Dense(ff_dim)], name=name
+        [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim)], name=name
     )
 
 
@@ -177,12 +177,15 @@ class Router(layers.Layer):
         expert_gate *= expert_mask_flat
         # Combine expert outputs and scaling with router probability.
         # combine_tensor shape: [tokens_per_batch, num_experts, expert_capacity]
-        combined_tensor = tf.expand_dims(
-            expert_gate
-            * expert_mask_flat
-            * tf.squeeze(tf.one_hot(expert_index, depth=self.num_experts), 1),
-            -1,
-        ) * tf.squeeze(tf.one_hot(position_in_expert, depth=self.expert_capacity), 1)
+        combined_tensor = (
+            tf.expand_dims(
+                expert_gate
+                * expert_mask_flat
+                * tf.squeeze(tf.one_hot(expert_index, depth=self.num_experts), 1),
+                -1,
+            )
+            * tf.squeeze(tf.one_hot(position_in_expert, depth=self.expert_capacity), 1)
+        )
         # Create binary dispatch_tensor [tokens_per_batch, num_experts, expert_capacity]
         # that is 1 if the token gets routed to the corresponding expert.
         dispatch_tensor = tf.cast(combined_tensor, tf.dtypes.float32)
@@ -196,11 +199,13 @@ class Router(layers.Layer):
 
 
 class Switch(layers.Layer):
-    def __init__(self, num_experts, embed_dim, num_tokens_per_batch, capacity_factor=1):
+    def __init__(
+        self, num_experts, embed_dim, ff_dim, num_tokens_per_batch, capacity_factor=1
+    ):
         self.num_experts = num_experts
         self.embed_dim = embed_dim
         self.experts = [
-            create_feedforward_network(embed_dim) for _ in range(num_experts)
+            create_feedforward_network(ff_dim, embed_dim) for _ in range(num_experts)
         ]
 
         self.expert_capacity = num_tokens_per_batch // self.num_experts
@@ -277,8 +282,8 @@ of it to classify text.
 
 
 def create_classifier():
-    switch = Switch(num_experts, embed_dim, num_tokens_per_batch)
-    transformer_block = TransformerBlock(ff_dim, num_heads, switch)
+    switch = Switch(num_experts, embed_dim, ff_dim, num_tokens_per_batch)
+    transformer_block = TransformerBlock(embed_dim // num_heads, num_heads, switch)
 
     inputs = layers.Input(shape=(num_tokens_per_example,))
     embedding_layer = TokenAndPositionEmbedding(

commit 349eb4c351d4b04f996bcb724fa504dcf25bb750

Fix lint error introduced in #669

examples/nlp/text_classification_with_switch_transformer.py
@@ -177,15 +177,12 @@ class Router(layers.Layer):
         expert_gate *= expert_mask_flat
         # Combine expert outputs and scaling with router probability.
         # combine_tensor shape: [tokens_per_batch, num_experts, expert_capacity]
-        combined_tensor = (
-            tf.expand_dims(
-                expert_gate
-                * expert_mask_flat
-                * tf.squeeze(tf.one_hot(expert_index, depth=self.num_experts), 1),
-                -1,
-            )
-            * tf.squeeze(tf.one_hot(position_in_expert, depth=self.expert_capacity), 1)
-        )
+        combined_tensor = tf.expand_dims(
+            expert_gate
+            * expert_mask_flat
+            * tf.squeeze(tf.one_hot(expert_index, depth=self.num_experts), 1),
+            -1,
+        ) * tf.squeeze(tf.one_hot(position_in_expert, depth=self.expert_capacity), 1)
         # Create binary dispatch_tensor [tokens_per_batch, num_experts, expert_capacity]
         # that is 1 if the token gets routed to the corresponding expert.
         dispatch_tensor = tf.cast(combined_tensor, tf.dtypes.float32)